93ebc94a09b631814868ca5402096bfcc2b3f249
hamzakamran <hamzakwebdev@gmail.com>
2023-11-08 15:01:51 -0600

    Nav

--- src/features/layout/Layout.jsx
+++ src/features/layout/Layout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { StyledBody } from "./Layout.styled";
-import SideNav from "./sideNav/SideNav";
 import Container from "../ui/container/Container";
 import LayoutTop from "./layoutTop/LayoutTop";
 
 const Layout = () => {
 	return (
 		<StyledBody>
-			<SideNav />
 			<section className="mainContent">
 				<LayoutTop />
 				<Container>
--- src/features/layout/Layout.styled.js
+++ src/features/layout/Layout.styled.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
 export const StyledBody = styled.main`
-	flex: 1;
 	display: flex;
-	flex-direction: row;
-	align-items: top;
+	flex-direction: column;
 
 	section.mainContent {
 		width: 100%;
